db7a1defdfab70f1c288246edab07827cd984681
maiksteffan <44241462+maiksteffan@users.noreply.github.com>
2021-12-05 23:29:10 +0100

    admin page updated

--- login/loginValidation.js
+++ login/loginValidation.js
@@ -18,3 +18,42 @@ function validate(){
         document.getElementById("submit").disabled = true;
     }
 }
+
+function check(){
+    let string = document.getElementById("password").value;
+    let hasLower = false;
+    let hasUpper = false;
+    let hasNum = false;
+    let hasSpecial = false;
+    let hasEightChars = false;
+
+    let counter = 0;
+    hasLower = /[a-z]/.test(string);
+    hasUpper = /[A-Z]/.test(string);
+    hasNum = /[0-9]/.test(string);
+    hasSpecial = /[.!@#$%^&*()_+=]/.test(string);
+    hasEightChars = string.length>=8;
+    let strength = [hasLower, hasUpper, hasNum, hasSpecial, hasEightChars];
+
+    strength.map( (s) => {
+        if(s){
+            counter++;
+            console.log(counter);
+        }
+    });
+    switch(counter){
+        case 0: document.getElementById("strength").innerText = "super super weak!";
+            break;
+        case 1: document.getElementById("strength").innerText = "super weak!";
+            break;
+        case 2: document.getElementById("strength").innerText = "weak!";
+            break;
+        case 3: document.getElementById("strength").innerText = "ok!";
+            break;
+        case 4: document.getElementById("strength").innerText = "medium!";
+            break;
+        case 5: document.getElementById("strength").innerText = "strong!";
+            break;
+    }
+
+}
